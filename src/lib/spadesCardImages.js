f811622e0d02f0b49bc6438736cd97e521304328
David Berna <davidhambern@gmail.com>
2026-06-05 06:43:02 +0000

    File changes

--- src/lib/spadesCardImages.js
+++ src/lib/spadesCardImages.js
@@ -19,10 +19,28 @@ const SPADES_IMAGES = {
   'Q': 'https://media.base44.com/images/public/6a1faf9539e2c1e12925ead8/e68cf06b4_Queen-spades.png',
 };
 
+const HEARTS_IMAGES = {
+  '2': 'https://media.base44.com/images/public/6a1faf9539e2c1e12925ead8/e310765ac_hearts-2.png',
+  '3': 'https://media.base44.com/images/public/6a1faf9539e2c1e12925ead8/c3377892f_hearts-3.png',
+  '4': 'https://media.base44.com/images/public/6a1faf9539e2c1e12925ead8/86f65cd37_hearts-4.png',
+  '5': 'https://media.base44.com/images/public/6a1faf9539e2c1e12925ead8/3072be389_hearts-5.png',
+  '6': 'https://media.base44.com/images/public/6a1faf9539e2c1e12925ead8/a1e31dbaf_hearts-6.png',
+  '7': 'https://media.base44.com/images/public/6a1faf9539e2c1e12925ead8/558934f0e_hearts-7.png',
+  '8': 'https://media.base44.com/images/public/6a1faf9539e2c1e12925ead8/561cdc8de_hearts-8.png',
+  '9': 'https://media.base44.com/images/public/6a1faf9539e2c1e12925ead8/cd985c7d9_hearts-9.png',
+  'J': 'https://media.base44.com/images/public/6a1faf9539e2c1e12925ead8/8dd0b4d5d_hearts-J.png',
+  'Q': 'https://media.base44.com/images/public/6a1faf9539e2c1e12925ead8/32394a79e_hearts-Q.png',
+  'K': 'https://media.base44.com/images/public/6a1faf9539e2c1e12925ead8/5119c6ee6_hearts-K.png',
+  'A': 'https://media.base44.com/images/public/6a1faf9539e2c1e12925ead8/0bcc16c68_hearts-A.png',
+};
+
 export function getCardImage(card) {
   if (!card) return null;
   if (card.suit === '♠') {
     return SPADES_IMAGES[card.value] || null;
+  }
+  if (card.suit === '♥') {
+    return HEARTS_IMAGES[card.value] || null;
   }
   return null;
 }
