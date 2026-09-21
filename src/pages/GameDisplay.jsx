3bff2f17814b7e90f44d7be37bcc1b299a3a160a
davidhambern-ship-it <davidhambern@gmail.com>
2026-09-21 19:30:07 -0400

    Make Game Display the Square Biz jingle audio source

--- src/pages/GameDisplay.jsx
+++ src/pages/GameDisplay.jsx
@@ -1055,7 +1055,7 @@ function SquareBizDisplay({ room }) {
       )}
 
       {phase === 'intro' && (
-        <SquareBizIntro gameState={state} now={clock} />
+        <SquareBizIntro gameState={state} now={clock} playAudio />
       )}
 
       {phase !== 'intro' && (
