7ba47f7be87a9137a72ec0c9041d3c390220068d
davidhambern-ship-it <davidhambern@gmail.com>
2026-09-21 00:43:05 -0400

    Route Preview Game Display

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,7 @@ import Login from '@/pages/Login';
 import ForgotPassword from '@/pages/ForgotPassword';
 import ResetPassword from '@/pages/ResetPassword';
 import TngOnboarding from '@/pages/TngOnboarding';
+import GameDisplay from '@/pages/GameDisplay';
 import { getPreviewTngProfile, isBase44Preview } from '@/lib/previewTngProfile';
 
 function HomeGate() {
@@ -152,6 +153,7 @@ const AuthenticatedApp = () => {
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
       <Route path="/onboarding" element={<TngOnboarding />} />
+      <Route path="/display" element={<GameDisplay />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/reset-password" element={<ResetPassword />} />
       <Route path="/" element={<HomeGate />} />
