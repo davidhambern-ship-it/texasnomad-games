c06b8e114fcd6954913e069a460889a1d7c58431
davidhambern-ship-it <davidhambern@gmail.com>
2026-09-21 02:40:40 -0400

    Wire live display and room-state API client

--- src/api/tngApi.js
+++ src/api/tngApi.js
@@ -6,6 +6,8 @@ import { migrationEndpoints } from '@/config/backendMigration';
  * @property {string} [method]
  * @property {unknown} [body]
  * @property {string} [deviceId]
+ * @property {string} [displayId]
+ * @property {string} [displayToken]
  * @property {boolean} [authenticated]
  */
 
@@ -21,7 +23,6 @@ export class TngApiError extends Error {
 
 /**
  * @param {string} path
- * @param {RequestOptions} [options]
  */
 function apiUrl(path) {
   const base = migrationEndpoints.tngApiUrl;
@@ -29,13 +30,29 @@ function apiUrl(path) {
   return `${base.replace(/\/$/, '')}${path.replace(/^\/api/, '')}`;
 }
 
-async function request(path, { method = 'GET', body, deviceId, authenticated = true } = {}) {
+/**
+ * @param {string} path
+ * @param {RequestOptions} [options]
+ */
+async function request(
+  path,
+  {
+    method = 'GET',
+    body,
+    deviceId,
+    displayId,
+    displayToken,
+    authenticated = true,
+  } = {},
+) {
   const headers = { Accept: 'application/json' };
 
   if (authenticated) {
     headers.Authorization = `Bearer ${await getTngAccessToken()}`;
   }
   if (deviceId) headers['X-TNG-Device-Id'] = deviceId;
+  if (displayId) headers['X-TNG-Display-Id'] = displayId;
+  if (displayToken) headers['X-TNG-Display-Token'] = displayToken;
   if (body !== undefined) headers['Content-Type'] = 'application/json';
 
   const response = await fetch(apiUrl(path), {
@@ -65,28 +82,82 @@ export const tngApi = Object.freeze({
       body: { displayName, handle },
     }),
   },
+
   devices: {
     create: ({ role, deviceLabel }) => request('/api/device-session', {
       method: 'POST',
       body: { role, deviceLabel },
     }),
   },
+
   host: {
-    startSession: (deviceId) => request('/api/host/session', { method: 'POST', deviceId }),
-    endSession: (deviceId) => request('/api/host/session', { method: 'DELETE', deviceId }),
-    createPairing: (deviceId) => request('/api/host/pairing', { method: 'POST', deviceId }),
+    startSession: (deviceId) => request('/api/host/session', {
+      method: 'POST',
+      deviceId,
+    }),
+
+    endSession: (deviceId) => request('/api/host/session', {
+      method: 'DELETE',
+      deviceId,
+    }),
+
+    createPairing: (deviceId) => request('/api/host/pairing', {
+      method: 'POST',
+      deviceId,
+    }),
+
     createRoom: (deviceId, gameId) => request('/api/host/room', {
       method: 'POST',
       deviceId,
       body: { gameId },
     }),
-    endRoom: (deviceId) => request('/api/host/room', { method: 'DELETE', deviceId }),
+
+    endRoom: (deviceId) => request('/api/host/room', {
+      method: 'DELETE',
+      deviceId,
+    }),
+
+    roomState: {
+      get: (deviceId) => request('/api/host/room-state', {
+        deviceId,
+      }),
+
+      update: (deviceId, { statePatch, command } = {}) => request('/api/host/room-state', {
+        method: 'PATCH',
+        deviceId,
+        body: {
+          ...(statePatch ? { statePatch } : {}),
+          ...(command ? { command } : {}),
+        },
+      }),
+    },
   },
+
   display: {
     pair: (code) => request('/api/display/pair', {
       method: 'POST',
       body: { code },
       authenticated: false,
     }),
+
+    state: ({ displayId, displayToken }) => request('/api/display/state', {
+      displayId,
+      displayToken,
+      authenticated: false,
+    }),
+  },
+
+  spades: {
+    host: {
+      get: (deviceId) => request('/api/spades/host', {
+        deviceId,
+      }),
+
+      action: (deviceId, action, payload = {}) => request('/api/spades/host', {
+        method: 'POST',
+        deviceId,
+        body: { action, ...payload },
+      }),
+    },
   },
 });
